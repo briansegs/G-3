22c9685e35670d9de624d6b0c383ed3b94d25f71
bsegers <briansegs@gmail.com>
2024-04-29 15:29:06 -0400

    setup: nextjs and packages

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../public/logo.svg";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center gradient__bg px-14 py-10">
+        <div className="flex justify-start items-center">
+            <Image src={logo} alt="logo" />
+
+            <ul className="flex text-white font-manrope text-2xl gap-8 mx-8 items-center font-semibold">
+                <Link href="/">
+                    <li className="hover:text-gray-400">Home</li>
+                </Link>
+                <Link href="/">
+                    <li className="hover:text-gray-400">About</li>
+                </Link>
+                <Link href="/">
+                    <li className="hover:text-gray-400">Blog</li>
+                </Link>
+                <Link href="/">
+                    <li className="hover:text-gray-400">Contact</li>
+                </Link>
+            </ul>
+        </div>
+
+        <div className="flex text-white font-manrope font-semibold text-2xl items-center justify-center gap-2">
+            <Link href='/'>
+                <div className="px-3">
+                    <p className="hover:text-gray-400">Sign In</p>
+                </div>
+            </Link>
+            
+            <Link href='/'>
+                <button 
+                type="button"
+                className="bg-red-600 hover:bg-[#245d9b] px-3 py-2 rounded-lg"
+                >
+                    Sign up
+                </button>
+            </Link>
+            
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <section className="flex justify-center items-center p-10">
+        <h1 className="text-3xl font-bold">Hello</h1>
+      </section>
+    </main>
+  );
+}
